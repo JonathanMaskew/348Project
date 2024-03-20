eb1bd4c622f2668d6c3bef598d860341bcd24907
Jonathan Maskew <jmaskew@purdue.edu>
2024-03-20 15:05:19 -0400

    mongodb template set up

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+
+const PORT = process.env.PORT || 5050;
 const app = express();
-const cors = require('cors');
-require('dotenv').config({ path: './config.env' });
-const port = process.env.PORT || 500;
+
 app.use(cors());
 app.use(express.json());
-app.use(require('./routes/record'));
-// get driver connection
-const dbo = require('./db/conn');
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
